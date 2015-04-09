698837277d06340c53731b7c4ce955f4b7f75d0f
y1笑而过 <504677840@qq.com>
2015-04-10 00:58:54 +0800

    First commit.

---
+++ src/com/scp/marqueetextview/MarqueeActivity.java
@@ -0,0 +1,11 @@
+package com.scp.marqueetextview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MarqueeActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.marquee_layout);
+	}
+}
---
+++ src/com/scp/marqueetextview/MarqueeText.java
@@ -0,0 +1,25 @@
+package com.scp.marqueetextview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MarqueeText extends TextView {
+
+	public MarqueeText(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public MarqueeText(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public MarqueeText(Context context) {
+		super(context);
+	}
+
+	public boolean isFocused() {
+		return true;
+	}
+
+}
